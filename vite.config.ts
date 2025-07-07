359b0467e5703e86881a64333bd78e64c194d822
Rothezee <rothalan83@gmail.com>
2025-07-07 15:17:36 -0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Esp32PanelControlReact/", 
   plugins: [react()],
   server: {
     proxy: {
